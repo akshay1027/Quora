12ea3be6fbb0f990089be2f1e735631f1df64612
akshay1027 <akshayar1027@gmail.com>
2021-01-03 23:44:56 +0530

    frontend setup over

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <h2>pec quora</h2>
+      <Header />
     </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import HomeIcon from '@material-ui/icons/Home';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
@@ -5,3 +6,10 @@ import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import LanguageIcon from '@material-ui/icons/Language';
 
+const Header = () => {
+    return(
+        <h1>Header</h1>
+    )
+};
+
+export default Header;
