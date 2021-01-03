4dbe408507d28f9f42526cd26f2600983381e54b
akshay1027 <akshayar1027@gmail.com>
2021-01-03 17:05:43 +0530

    material ui icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,1 +1,7 @@
 import HomeIcon from '@material-ui/icons/Home';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import LanguageIcon from '@material-ui/icons/Language';
+
