e27e9d14c82b73f08700a39af4a3164d9f6bff0c
akshay1027 <akshayar1027@gmail.com>
2021-01-06 01:57:22 +0530

    updated Header.js

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,10 +5,47 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import LanguageIcon from '@material-ui/icons/Language';
+import SearchIcon from '@material-ui/icons/Search';
+import { Avatar } from "@material-ui/core";
 
 const Header = () => {
     return(
         <div className="header">
+
+            {/*left side */}
+
+         <div className="header_left">
+
+            <div className="left_logo">
+                   <h4 className="company logo"></h4>
+            </div>
+            <div className="left_home">
+                <HomeIcon />
+                <h4>Home</h4>
+            </div>
+            <div className="left_notification">
+                <NotificationsIcon />
+                <h4>Notifications</h4>
+            </div>
+        
+          </div>
+
+           {/*center side */}
+
+           <div className="header_center">
+               <input type="text" placeholder="search" />
+               <SearchIcon />
+           </div>
+
+           {/*right side */}
+
+           <div className="header_right">
+               <div className="right_user">
+                   <Avatar src=" " alt="user profile" />
+               </div>
+               <LanguageIcon />
+               <button>Ask Questions</button>
+           </div>
 
         </div>
             
