f0a7b4e7033365fb20b6a6f8b8dda2e33a608da0
akshay1027 <akshayar1027@gmail.com>
2021-01-06 18:14:22 +0530

    question box added in home page

--- client/src/components/QuestionBox.jsx
+++ client/src/components/QuestionBox.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Avatar from "@material-ui/core/Avatar";
-
+import "../StyleSheet/QuestionBox.css";
 
 const QuestionBox = () => {
     return (
         <div className="QuestionBox">
-            Question
+            <div className="questionBox_user">
+                <Avatar src="" 
+                alt="user profile" />
+                <h4 className="user_username">Akshay</h4>
+            </div>
+
+            <input type="text" 
+            placeholder="What is your question ?" 
+            className="QuestionBox_inputfield"
+            />
         </div>
     )
 }
