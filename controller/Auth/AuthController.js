5270583da517c78e33734562ce7d372261b30bad
akshay1027 <akshayar1027@gmail.com>
2021-01-22 07:31:06 +0530

    Update AuthController.js

--- controller/Auth/AuthController.js
+++ controller/Auth/AuthController.js
@@ -6,3 +6,11 @@ import userSessions from "../../Model/UserSession/UserSessions";
 import Bcrypt from "bcrypt";
 
 dotenv.config();
+
+const cloudinary = Cloudinary.v2;
+
+cloudinary.config ({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
+});
