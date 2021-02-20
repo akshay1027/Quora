547f1ae762b9ddab25ba57e0a1ee0821d10a1fbb
akshay1027 <akshayar1027@gmail.com>
2021-02-20 08:28:54 +0530

    writing to db, signup logic done!

--- controller/Auth/AuthController.js
+++ controller/Auth/AuthController.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import formidable from 'formidable';
 import Cloudinary from 'cloudinary';
-import userModel from "../../Model/User/Users";
-import userSessions from "../../Model/UserSession/UserSessions";
+import {userModel} from "../../Model/User/Users";
+import {userSessions} from "../../Model/UserSession/UserSessions";
 import Bcrypt from "bcrypt";
 
 dotenv.config();
@@ -19,14 +19,16 @@ cloudinary.config ({
 //----------------class method to follow OOPs-------------------
 
 class AuthController{
-   
+    
+    // signup logic
+
     signup(request,response) {
         
         const form = new formidable.IncomingForm();
 
         try {
 
-            form.parse(request, (error, fields, files)=>{
+            form.parse(request, async (error, fields, files)=>{
                 if(error){
                     console.error(error);
                     return response
@@ -54,18 +56,70 @@ class AuthController{
                 }
 
                 //check for duplicate username
+                //while checking with db we use async await
+                //else not needed
 
+                const isUserExisting = await userModel.findOne({ username: username});
+                
+                // if user exists, return error
 
+                if(isUserExisting){
+                    return response
+                        .status(404)
+                        .json({ msg:"User with this username already exists"});
+                }
+                
+                //upload image to cloudinary to get url of the image
 
+                cloudinary.uploader.upload(image.path, {folder:`/pecquora/profileimage/${username}`}, async (error,results)=>{
+                    if(error){
+                        console.error(error);
+                        return response
+                            .status(500)
+                            .json({ msg:"Image upload error: check your image file type"});
+                    }
+                    
+                    // get url of the image
+
+                    const image_url = results.secure_url;
+
+                    // Hash or encrypt password(best parctise) using Bcrypt
+                    // salt is used to encrypt password
+
+                    const salt = await Bcrypt.genSalt(15);
+                    const hashedPassword = await Bcrypt.hash(password,salt);
+
+                    // create new instance and write/save to db
+
+                    const newUser = new userModel({
+                        username: username,
+                        password: hashedPassword,
+                        profileImage: image_url
+                    })
+                    
+                    // everytime you interact with mongoDB, use async await
+
+                    const savedUser = await newUser.save();
+                    
+                    // 201-created, 200-okay
+
+                    return response
+                        .status(201)
+                        .json({ msg:"New account has been created"})
+
+                });
+               
             });
 
-        } catch(error){
+        } 
+        
+        catch(error){
             return response
             .status(500)
             .json({ msg: "Server currently down, try later"});
         }
-        // use try catch becoz it returns error 
-        //when the server is down but the try logic is correct
+        // use try catch becoz it returns error even 
+        // when the server is down but the try logic is correct.
         // status(500) indicates internal server error
 
     }
