64c1482398e06c93f5a0f911743d06b022d3c269
akshay1027 <akshayar1027@gmail.com>
2021-02-23 07:59:44 +0530

    authentication Routes

---
+++ Routes/AuthRoute/AuthRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authController from "../../Controller/Auth/AuthController";
+
+const router = express.Router();
+const AuthController = new authController();
+
+router.post("/signup", (request, response) => {
+  AuthController.SignUp(request, response);
+});
+
+router.post("/signin", (request, response) => {
+  AuthController.Login(request, response);
+});
+
+router.get("/isUserLoggedIn", (request, response) => {
+  AuthController.isLoggedIn(request, response);
+});
+
+export default router;
