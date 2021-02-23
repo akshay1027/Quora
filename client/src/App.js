116b31c17ca753a246414eb0bee2db891f681542
akshay1027 <akshayar1027@gmail.com>
2021-02-23 07:57:36 +0530

    signin.jsx done

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import Header from "./components/Header";
 import QuestionBox from "./components/QuestionBox";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import SignUp from "./components/SignUp";
-
+import SignIn from "./components/SignIn";
 function App() {
   return (
     <Router className="App">
       <Header /> {/* header will be present in all pages */}
       <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
           <Route path="/signup">
             <SignUp />
           </Route>
