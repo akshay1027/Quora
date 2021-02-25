db4c407a38543fa9bd7b620a22d4f998be6ce1f1
akshay1027 <akshayar1027@gmail.com>
2021-02-26 05:06:40 +0530

    Update QuestionBox.jsx

--- client/src/components/QuestionBox.jsx
+++ client/src/components/QuestionBox.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Avatar from "@material-ui/core/Avatar";
+import axios from "axios";
 import "../StyleSheet/QuestionBox.css";
 
-const QuestionBox = () => {
+const QuestionBox = ({profileimage, authStatus}) => {
+    
+    const [question, setQuestion] = useState("");
+    
+    const AskQuestion = async () => {
+        const form_data = new FormData();
+        form_data.append("question", question);
+        
+        const url = "http://localhost:5000/ask-question";
+        
+        try {
+        const response = await axios.post(url, form_data, {
+            withCredentials: true,
+        });
+        
+        alert(response.data.msg);
+        } catch (error) {
+        alert(error.response.data.msg);
+        }
+    };
+    
     return (
         <div className="QuestionBox">
             <div className="questionBox_user">
-                <Avatar src="" 
+                <Avatar src={profileimage} 
                 alt="user profile" />
                 <h4 className="user_username">Akshay</h4>
             </div>
-
-            <input type="text" 
-            placeholder="What is your question ?" 
-            className="QuestionBox_inputfield"
-            />
+            
+            <div className="QuestionBox__inputField">
+                <input type="text" 
+                placeholder="What is your question ?" 
+                className="QuestionBox__inputfield"
+                />
+                <button
+                disabled={authStatus === true ? false : true} //allow to ask question only if user is logged in!
+                className="QuestionBox__btn"
+                onClick={AskQuestion}
+                >
+                Ask Question
+                </button>
+            </div>
         </div>
     )
 }
 
-export default QuestionBox
+export default QuestionBox;
