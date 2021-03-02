05fe98163115c11a17cd93883bd5176a867f95be
akshay1027 <akshayar1027@gmail.com>
2021-03-02 20:54:41 +0530

    DRY, take questions code and use here

--- client/src/components/AnswerBox.jsx
+++ client/src/components/AnswerBox.jsx
@@ -1,29 +1,12 @@
 import React,{useState} from 'react';
 import Avatar from "@material-ui/core/Avatar";
-import "../StyleSheet/QuestionBox.css";
 import axios from "axios";
 import "../StyleSheet/QuestionBox.css";
 
-const QuestionBox = ({profileimage, authStatus}) => {
+const AnswerBox = ({profileimage, authStatus}) => {
     
     const [question, setQuestion] = useState("");
     
-    const AskQuestion = async () => {
-        const form_data = new FormData();
-        form_data.append("question", question);
-        
-        const url = "http://localhost:5000/ask-question";
-        
-        try {
-        const response = await axios.post(url, form_data, {
-            withCredentials: true,
-        });
-        
-        alert(response.data.msg);
-        } catch (error) {
-        alert(error.response.data.msg);
-        }
-    };
     
     return (
         <div className="QuestionBox">
@@ -35,7 +18,7 @@ const QuestionBox = ({profileimage, authStatus}) => {
             
             <div className="QuestionBox_inputField">
                 <input type="text" 
-                placeholder="What is your question ?" 
+                placeholder="What is your answer ?" 
                 className="QuestionBox_inputfield"
                 onChange={(e) => setQuestion(e.target.value)}
                 value={question}
@@ -43,13 +26,13 @@ const QuestionBox = ({profileimage, authStatus}) => {
                 <button
                 disabled={authStatus === true ? false : true} //allow to ask question only if user is logged in!
                 className="QuestionBox__btn"
-                onClick={AskQuestion}
+               
                 >
-                Ask Question
+                Answer
                 </button>
             </div>
         </div>
     )
 }
 
-export default QuestionBox;
+export default AnswerBox;
