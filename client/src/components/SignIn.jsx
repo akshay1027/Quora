d38c8765936fe3370289b9b54511856792004137
akshay1027 <akshayar1027@gmail.com>
2021-04-21 21:39:29 +0530

    Update SignIn.jsx

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -10,7 +10,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import axios from "axios";
+import axios from "../helper/axioshelper";
 
 
 function Copyright() {
@@ -67,7 +67,7 @@ export default function SignUp() {
   
   
       try {
-        const response = await axios.post(url, form_data, {
+        const response = await axios.post("/signin", form_data, {
           withCredentials: true,
         });
         console.log(response);
