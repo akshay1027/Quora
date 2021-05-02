d9e837c9fa975ce0ab82261c329b9b1c5331b344
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-05-02 11:40:23 +0530

    updated

---
+++ Model/Questions.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const questionSchema = mongoose.Schema({
+    owner: { type: String, required:true },
+    owner_image: { type: String, required:true },
+    question: { type: String, required:true },
+    upvotes: { type: Number, default:0 },
+    comments: { type: Array, default:[] },
+});
+
+const questionModel = mongoose.model("questionsModel", questionSchema);
+
+export default questionModel;
