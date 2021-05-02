341b6cce9768e247597f36f21c7f610cba9484ec
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-05-02 11:42:01 +0530

    Delete Model/User directory

--- Model/User/Users.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    profileImage: {type: String}
-});
-
-export const userModel = mongoose.model("userModal", userSchema);
